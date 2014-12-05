c9ec50b5c2a976540e4c2b81ef3921dddaddb3b6
Christian <lalanne@gmail.com>
2014-12-05 01:23:03 +0100

    extracting classes to its own cpp files, erasing unused includes pragmas

---
+++ MyFrontendAction.cpp
@@ -0,0 +1,26 @@
+
+#include "MyFrontendAction.hpp"
+
+using namespace clang;
+using namespace std;
+
+MyFrontendAction::MyFrontendAction(){}
+
+void MyFrontendAction::EndSourceFileAction(){
+    SourceManager &SM = myRewriter.getSourceMgr();
+    llvm::errs() << "** EndSourceFileAction for: "
+         << SM.getFileEntryForID(SM.getMainFileID())->getName() << "\n";
+
+    // Now emit the rewritten buffer.
+    myRewriter.getEditBuffer(SM.getMainFileID()).write(llvm::outs());
+} 
+
+ASTConsumer*  MyFrontendAction::CreateASTConsumer(CompilerInstance &CI,
+                                                StringRef file) override {
+    llvm::errs() << "** Creating AST consumer for: " << file << "\n";
+    myRewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
+    //return llvm::make_unique<MyASTConsumer>(myRewriter);
+    return new MyASTConsumer(myRewriter); //danger!!!! awful leak!!!!
+}
+
+
--- MyFrontendAction.hpp
+++ MyFrontendAction.hpp
@@ -1,27 +1,18 @@
 
-#include "MyASTConsumer.hpp"
-
+#include "clang/AST/ASTConsumer.h"
 #include "clang/Frontend/FrontendAction.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+#include "clang/Basic/SourceManager.h"
 
 class MyFrontendAction : public clang::ASTFrontendAction {
     public:
-        MyFrontendAction() {}
-        void EndSourceFileAction() override {
-            clang::SourceManager &SM = myRewriter.getSourceMgr();
-            llvm::errs() << "** EndSourceFileAction for: "
-                 << SM.getFileEntryForID(SM.getMainFileID())->getName() << "\n";
+        MyFrontendAction();
 
-            // Now emit the rewritten buffer.
-            myRewriter.getEditBuffer(SM.getMainFileID()).write(llvm::outs());
-        } 
+        void EndSourceFileAction() override;
 
-        /*std::unique_ptr<ASTConsumer>*/clang::ASTConsumer*  CreateASTConsumer(clang::CompilerInstance &CI,
-                                                 clang::StringRef file) override {
-            llvm::errs() << "** Creating AST consumer for: " << file << "\n";
-            myRewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
-            //return llvm::make_unique<MyASTConsumer>(myRewriter);
-            return new MyASTConsumer(myRewriter); //danger!!!! awful leak!!!!
-        }
+        /*std::unique_ptr<ASTConsumer>*/
+        clang::ASTConsumer*  CreateASTConsumer(clang::CompilerInstance &CI,
+                                            llvm::StringRef file) override;
 
     private:
         clang::Rewriter myRewriter;
