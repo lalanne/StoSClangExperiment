d709f80ea15271cab249c86a59b0bfd46caeac19
Christian <lalanne@gmail.com>
2014-12-08 15:18:08 +0100

    ADD: more tests

--- tst/tests.py
+++ tst/tests.py
@@ -61,3 +61,25 @@ def test_basic_static_1():
     destroy_output_cpp_file(RESULT_CPP_FILE_NAME)
 
 
+def test_basic_static_2():
+    testFileName = "test_omp2.cpp"
+
+    create_output_cpp_file(RESULT_CPP_FILE_NAME, execute_binary(testFileName))
+    resultFile = read_result_file()
+    expectedOutputFile = read_expected_result_file(testFileName)
+
+    assert expectedOutputFile == resultFile
+
+    destroy_output_cpp_file(RESULT_CPP_FILE_NAME)
+
+def test_basic_static_3():
+    testFileName = "test_omp3.cpp"
+
+    create_output_cpp_file(RESULT_CPP_FILE_NAME, execute_binary(testFileName))
+    resultFile = read_result_file()
+    expectedOutputFile = read_expected_result_file(testFileName)
+
+    assert expectedOutputFile == resultFile
+
+    destroy_output_cpp_file(RESULT_CPP_FILE_NAME)
+
