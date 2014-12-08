b3057b1e808e658b2deac7d00eca32017b4f7dc7
Christian <lalanne@gmail.com>
2014-12-08 14:30:40 +0100

    ADD: adding another test to the suit

--- tst/tests.py
+++ tst/tests.py
@@ -9,9 +9,9 @@ def create_output_cpp_file(nameOfFile, commandOutput):
 def destroy_output_cpp_file(nameOfFile):
     os.remove(nameOfFile)
 
-def execute_binary():
+def execute_binary(inputFile):
     args = ("/Users/lalanne/clang-llvm-omp/build/bin/sts", 
-            "input/test_omp.cpp", 
+            "input/"+inputFile, 
             "--", 
             "-fopenmp")
     popen = subprocess.Popen(args, stdout=subprocess.PIPE)
@@ -21,13 +21,28 @@ def execute_binary():
 
 
 def test_basic_static():
-    execute_binary()
-    create_output_cpp_file("output.cpp", execute_binary())
+    create_output_cpp_file("output.cpp", execute_binary("test_omp.cpp"))
 
     resultHandler = open('output.cpp', 'r')
     resultFile = resultHandler.read()
 
     expectedOutputFileHandler = open('expected_output/test_omp.cpp', 'r')
+    expectedOutputFile = expectedOutputFileHandler.read()
+
+    assert expectedOutputFile == resultFile
+
+    resultHandler.close()
+    expectedOutputFileHandler.close()
+
+    destroy_output_cpp_file("output.cpp")
+
+def test_basic_static_1():
+    create_output_cpp_file("output.cpp", execute_binary("test_omp1.cpp"))
+
+    resultHandler = open('output.cpp', 'r')
+    resultFile = resultHandler.read()
+
+    expectedOutputFileHandler = open('expected_output/test_omp1.cpp', 'r')
     expectedOutputFile = expectedOutputFileHandler.read()
 
     assert expectedOutputFile == resultFile
