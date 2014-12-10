763a41c6f4aee119236de7ee68962c5f93dbb592
Christian <lalanne@gmail.com>
2014-12-10 19:03:22 +0100

    ADD: more tests

--- tst/tests.py
+++ tst/tests.py
@@ -110,5 +110,15 @@ def test_basic_dynamic():
 
     destroy_output_cpp_file(RESULT_CPP_FILE_NAME)
 
+def test_basic_dynamic_2():
+    testFileName = "test_omp_dynamic2.cpp"
+
+    create_output_cpp_file(RESULT_CPP_FILE_NAME, execute_binary(testFileName))
+    resultFile = read_result_file()
+    expectedOutputFile = read_expected_result_file(testFileName)
+
+    assert expectedOutputFile == resultFile
+
+    destroy_output_cpp_file(RESULT_CPP_FILE_NAME)
 
 
