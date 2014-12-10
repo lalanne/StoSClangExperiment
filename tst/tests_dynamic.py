98b0820587bb1fd1886eca60174ba39acd267af0
Christian <lalanne@gmail.com>
2014-12-10 22:08:20 +0100

    ADD: separating function in files

--- tst/tests_dynamic.py
+++ tst/tests_dynamic.py
@@ -1,8 +1,8 @@
 
 from helpers import *
 
-def test_basic_static():
-    testFileName = "test_omp.cpp"
+def test_basic_dynamic():
+    testFileName = "test_omp_dynamic.cpp"
 
     create_output_cpp_file(RESULT_CPP_FILE_NAME, execute_binary(testFileName))
     resultFile = read_result_file()
@@ -12,8 +12,8 @@ def test_basic_static():
 
     destroy_output_cpp_file(RESULT_CPP_FILE_NAME)
 
-def test_basic_static_1():
-    testFileName = "test_omp1.cpp"
+def test_basic_dynamic_2():
+    testFileName = "test_omp_dynamic2.cpp"
 
     create_output_cpp_file(RESULT_CPP_FILE_NAME, execute_binary(testFileName))
     resultFile = read_result_file()
@@ -23,20 +23,8 @@ def test_basic_static_1():
 
     destroy_output_cpp_file(RESULT_CPP_FILE_NAME)
 
-
-def test_basic_static_2():
-    testFileName = "test_omp2.cpp"
-
-    create_output_cpp_file(RESULT_CPP_FILE_NAME, execute_binary(testFileName))
-    resultFile = read_result_file()
-    expectedOutputFile = read_expected_result_file(testFileName)
-
-    assert expectedOutputFile == resultFile
-
-    destroy_output_cpp_file(RESULT_CPP_FILE_NAME)
-
-def test_basic_static_3():
-    testFileName = "test_omp3.cpp"
+def test_basic_dynamic_3():
+    testFileName = "test_omp_dynamic3.cpp"
 
     create_output_cpp_file(RESULT_CPP_FILE_NAME, execute_binary(testFileName))
     resultFile = read_result_file()
@@ -46,8 +34,8 @@ def test_basic_static_3():
 
     destroy_output_cpp_file(RESULT_CPP_FILE_NAME)
 
-def test_basic_static_4():
-    testFileName = "test_omp4.cpp"
+def test_basic_dynamic_4():
+    testFileName = "test_omp_dynamic4.cpp"
 
     create_output_cpp_file(RESULT_CPP_FILE_NAME, execute_binary(testFileName))
     resultFile = read_result_file()
